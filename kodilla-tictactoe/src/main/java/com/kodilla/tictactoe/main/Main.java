c1d982d81a9642427ae05e88809254ed4df1406e
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-05-08 23:13:35 +0200

    new main class created

---
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/main/Main.java
@@ -0,0 +1,20 @@
+package com.kodilla.tictactoe.main;
+
+import com.kodilla.tictactoe.components.Board;
+import com.kodilla.tictactoe.components.Figure;
+import com.kodilla.tictactoe.components.ShowBoard;
+
+public class Main {
+    public static void main(String[] args) {
+        Board board = new Board(3);
+
+        board.setValue(1, Figure.X);
+        board.setValue(2, Figure.O);
+        board.setValue(5, Figure.X);
+
+//        board.setValue(55, Figure.X);
+//        board.setValue(100, Figure.O);
+
+        ShowBoard.showBoard(board);
+    }
+}

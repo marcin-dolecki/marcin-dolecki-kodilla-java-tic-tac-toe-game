15d72099c67b0808c0bb28df26c7366ffd1f91eb
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-05-10 23:00:14 +0200

    New game logic added

--- kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/components/GameLogic.java
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/components/GameLogic.java
@@ -17,5 +17,46 @@ public class GameLogic {
         this.movesMade = 0;
     }
 
+    public LogicReturn makeMove(int row, int col, Figure figure) {
+        if (board.getValue(row, col) != Figure.EMPTY) return LogicReturn.FIELD_TAKEN;
+        board.setValue(row, col, figure);
+        movesMade++;
+        return LogicReturn.MOVE_ADDED;
+    }
 
+    public boolean checkWin(int row, int col, Figure figure) {
+        return checkDirection(row, col, figure, 0, 1) ||
+                checkDirection(row, col, figure, 1, 0) ||
+                checkDirection(row, col, figure, 1, 1) ||
+                checkDirection(row, col, figure, 1, -1);
+    }
+
+    public boolean isDraw() {
+        return movesMade == boardSideSize * boardSideSize;
+    }
+
+    public Board getBoard() {
+        return board;
+    }
+
+    private boolean checkDirection(int row, int col, Figure figure, int rowDirection, int colDirection) {
+//        int count = 1;
+        int count = 0;
+        count += countInDirection(row, col, figure, rowDirection, colDirection);
+        count += countInDirection(row, col, figure, -rowDirection, -colDirection);
+        return count >= winMoveLength;
+    }
+
+    private int countInDirection(int row, int col, Figure figure, int rowDirection, int colDirection) {
+        int count = 0;
+//        row += rowDirection;
+//        col += colDirection;
+
+        while (row >= 0 && row < boardSideSize && col >= 0 && col < boardSideSize && board.getValue(row, col) == figure) {
+            count++;
+            row += rowDirection;
+            col += colDirection;
+        }
+        return count;
+    }
 }
--- kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/main/Main.java
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/main/Main.java
@@ -2,19 +2,28 @@ package com.kodilla.tictactoe.main;
 
 import com.kodilla.tictactoe.components.Board;
 import com.kodilla.tictactoe.components.Figure;
+import com.kodilla.tictactoe.components.GameLogic;
 import com.kodilla.tictactoe.components.ShowBoard;
 
 public class Main {
     public static void main(String[] args) {
-        Board board = new Board(10);
-
-        board.setValue(1, 1, Figure.X);
-        board.setValue(1, 2, Figure.O);
-        board.setValue(2, 2, Figure.X);
+//        Board board = new Board(10);
+//
+//        board.setValue(1, 1, Figure.X);
+//        board.setValue(1, 2, Figure.O);
+//        board.setValue(2, 2, Figure.X);
+//        board.setValue(10, 10, Figure.O);
 
 //        board.setValue(55, Figure.X);
 //        board.setValue(100, Figure.O);
 
-        System.out.println(ShowBoard.showBoard(board));
+
+
+        GameLogic gameLogic = new GameLogic(3, 3);
+
+        gameLogic.makeMove(1, 1, Figure.X);
+        gameLogic.makeMove(2, 2, Figure.O);
+
+        System.out.println(ShowBoard.showBoard(gameLogic.getBoard()));
     }
 }

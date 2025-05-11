0b6fbb5b49d5ca0e83407a9cb2a7cf7e6e05edb2
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-05-11 22:42:36 +0200

    Player class created

---
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/components/Player.java
@@ -0,0 +1,38 @@
+package com.kodilla.tictactoe.components;
+
+import java.util.Objects;
+
+public class Player {
+    private final Figure figure;
+    private final boolean isComputerPlayer;
+
+    public Player(Figure figure, boolean isComputerPlayer) {
+        this.figure = figure;
+        this.isComputerPlayer = isComputerPlayer;
+    }
+
+    public Player(Figure figure) {
+        this(figure, false);
+    }
+
+    public Figure getFigure() {
+        return figure;
+    }
+
+    public boolean isComputerPlayer() {
+        return isComputerPlayer;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Player player = (Player) o;
+        return isComputerPlayer == player.isComputerPlayer && figure == player.figure;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(figure, isComputerPlayer);
+    }
+}

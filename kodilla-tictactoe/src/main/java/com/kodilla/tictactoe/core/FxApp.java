a5b7a71b74b6a54927bb107ae6add447cad01d69
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-22 20:50:30 +0200

    fx app finished

--- kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/core/FxApp.java
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/core/FxApp.java
@@ -1,4 +1,40 @@
 package com.kodilla.tictactoe.core;
 
-public class FxApp {
+import com.kodilla.tictactoe.logic.ExitRequestedException;
+import com.kodilla.tictactoe.ui.ComputerPlayerInterface;
+import com.kodilla.tictactoe.ui.JavaFxDisplay;
+import com.kodilla.tictactoe.ui.RandomMove;
+import com.kodilla.tictactoe.ui.TicTacToeController;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
+
+public class FxApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+        BorderPane root = new BorderPane();
+        GridPane grid = new GridPane();
+
+        TicTacToeController controller = new TicTacToeController(root, grid);
+        JavaFxDisplay ui = new JavaFxDisplay(controller);
+        ComputerPlayerInterface computerPlayer = new RandomMove();
+
+        Game game = new Game(ui, computerPlayer);
+
+        Scene scene = new Scene(root, 800, 800);
+        stage.setScene(scene);
+        stage.setTitle("Tic Tac Toe");
+        stage.show();
+
+        new Thread(() -> {
+            try {
+                ui.showMainMenu();
+                game.start();
+            } catch (ExitRequestedException ignored) {
+                // do nothing
+            }
+        }).start();
+    }
 }

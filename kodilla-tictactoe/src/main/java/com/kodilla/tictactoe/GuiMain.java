35f26c30eb7b767d03d3aea61c603c34af6ac528
marcin-dolecki <marcin.dolecki.md@gmail.com>
2025-08-22 20:54:38 +0200

    gui main finished

--- kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/GuiMain.java
+++ kodilla-tictactoe/src/main/java/com/kodilla/tictactoe/GuiMain.java
@@ -1,7 +1,9 @@
 package com.kodilla.tictactoe;
+import com.kodilla.tictactoe.core.FxApp;
+import javafx.application.Application;
 
 public class GuiMain {
     public static void main(String[] args) {
-
+        Application.launch(FxApp.class, args);
     }
 }
